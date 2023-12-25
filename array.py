d9184df7814037160f7cbaab6fcc9a18bf4be610
creation-extro <139437741+creation-extro@users.noreply.github.com>
2023-12-25 10:15:27 +0530

    Add files via upload
    this is  my first code on array

---
+++ array.py
@@ -0,0 +1,14 @@
+import array as arr
+a=arr.array ('i',[1,2,3,4,5])
+print(a)
+print (a[2])
+#negative indexing
+print(a[-1])
+print(a[-2])
+print(a[-3])
+print(a[-4])
+# range
+print(a[2:4])
+print(a[2:])
+print(a[:2])
+
