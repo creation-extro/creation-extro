72c0290571decebfe7e6698d001d0acaea82e882
creation-extro <139437741+creation-extro@users.noreply.github.com>
2023-12-25 10:18:07 +0530

    Add files via upload
    these are conditional statements for basic

---
+++ conditional statment pratice1.py
@@ -0,0 +1,33 @@
+
+
+
+
+#2
+"""num = int((input("enter your num")))
+if num%2==0:
+    print("no is even ")
+else:
+    print("no is even ")"""
+
+#3
+"""a = int((input("enter your bio percentage ")))
+b = int((input("enter your it percentage ")))
+c =a + b
+e =c/200
+f =e * 100
+print ("your overall percentage " , f)
+if f>= 75:
+    print("you have got A grade ")
+elif f>=54:
+    print("ypu have got B grade ")
+elif f<= 34:
+    print ("you have fAail")"""
+
+
+
+a = ["apple", " mango ", "banana"]
+
+
+
+    
+
---
+++ if elif else.py
@@ -0,0 +1,8 @@
+pincode = int(input("Enter you pincode"))
+city = str(input("Enter your city "))
+if  pincode>=40001 <=40005:
+    print("you are eligible ")
+elif city == "mumbai" :
+    print(" you are eligble ")
+else:
+    print("your are not ")
